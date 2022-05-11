750fe5ed5c8c638db2c18ea9e1fff9476e4a1aea
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2022-05-11 12:23:44 +0530

    sec 6 - vid 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const GrandChild = (props) => {
+  return(
+    <div>
+      <h3>GrandChild: </h3>
+      <Child brand={props.brand}/>
     </div>
   );
-}
+};
+
+const Child = (props) => {
+  return(
+    <div>
+      <h2>Child:{props.brand} </h2>
+    </div>
+  );
+};
+
+const App = () => {
+  const [brandname]=useState("Amazon");
+
+  return(
+    <div>
+      <h1>Hello</h1>
+      
+      <GrandChild brand={brandname}/>
+    </div>
+  );
+};
 
 export default App;
+
